b3e98114f6d7893e7d5a4f4d89212e71f1e5c4d0
Lucy W. Mwangi <100368317+lucy-sees@users.noreply.github.com>
2023-11-11 20:25:16 +0300

    Update person.rb

--- person.rb
+++ person.rb
@@ -1,37 +1,31 @@
 require './nameable'
 require './capitalize_decorator'
 require './trimmer_decorator'
-require './rental'
 
 class Person < Nameable
-  attr_reader :id
-  attr_accessor :age, :name, :rental
+  attr_accessor :name, :age
+  attr_reader :id, :rentals
 
-  def initialize(age, name: 'Unknown', parent_permission: true)
+  def initialize(age, name = 'Unknown', parent_permission: true)
+    super()
     @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
-    @rental = []
-    super()
+    @rentals = []
   end
 
   def can_use_services?
-    of_age? || @parent_permission
+    @age >= 18 || @parent_permission
   end
 
   def correct_name
     @name
   end
 
-  def add_rental(rental)
-    @rental.push(rental)
-    rental.person = self
-  end
-
   private
 
   def of_age?
-    @age >= 18
+    @age && @age >= 18
   end
 end
