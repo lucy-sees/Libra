68c112d8ece61e47ea81aa8ea313f566013cbe05
Lucy W. Mwangi <100368317+lucy-sees@users.noreply.github.com>
2023-11-11 13:12:58 +0300

    Update main.rb

--- main.rb
+++ main.rb
@@ -2,13 +2,13 @@ require './app'
 
 def show_menu
   puts "\nPlease choose an option by entering a number:
-  1 - List all books
-  2 - List all people
-  3 - Create a person
-  4 - Create a book
-  5 - Create a rental
-  6 - List all rentals for a given person id
-  7 - Exit"
+1 - List all books
+2 - List all people
+3 - Create a person
+4 - Create a book
+5 - Create a rental
+6 - List all rentals for a given person id
+7 - Exit"
 end
 
 def process_options(usr_inp)
@@ -16,15 +16,15 @@ def process_options(usr_inp)
   when '1'
     @app.list_all_books
   when '2'
-    puts '2'
+    @app.list_all_people
   when '3'
-    puts '3'
+    @app.create_person
   when '4'
     @app.create_book
   when '5'
-    puts '5'
+    @app.create_rental
   when '6'
-    puts '6'
+    @app.list_rentals
   else
     exit
   end
@@ -36,6 +36,7 @@ def main
   puts 'Welcome to School Library App!'
   loop do
     show_menu
+    user_input = gets.chomp
     if user_input == '7'
       puts 'Thank you for using this app!'
       break
