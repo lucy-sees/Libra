4d1d08152967224789aa9ad6857d1924612f171d
Lucy W. Mwangi <100368317+lucy-sees@users.noreply.github.com>
2023-11-11 20:24:49 +0300

    Update main.rb

--- main.rb
+++ main.rb
@@ -1,47 +1,84 @@
 require './app'
+app = App.new
 
-def show_menu
-  puts "\nPlease choose an option by entering a number:
-1 - List all books
-2 - List all people
-3 - Create a person
-4 - Create a book
-5 - Create a rental
-6 - List all rentals for a given person id
-7 - Exit"
+def main(app)
+  display_options
+  number = gets.chomp.to_i
+  execute_option(app, number)
 end
 
-def process_options(usr_inp)
-  case usr_inp
-  when '1'
-    @app.list_all_books
-  when '2'
-    @app.list_all_people
-  when '3'
-    @app.create_person
-  when '4'
-    @app.create_book
-  when '5'
-    @app.create_rental
-  when '6'
-    @app.list_rentals
+def display_options
+  puts
+  puts 'Please choose an option by entering a number:'
+  puts '1 - List all books'
+  puts '2 - List all people'
+  puts '3 - Create a person'
+  puts '4 - Create a book'
+  puts '5 - Create a rental'
+  puts '6 - List all rentals for a given person id'
+  puts '7 - Exit'
+end
+
+def execute_option(app, number)
+  option_methods = {
+    1 => :list_books,
+    2 => :list_persons,
+    3 => :create_person_option,
+    4 => :create_book_option,
+    5 => :create_rental,
+    6 => :get_rental_option,
+    7 => :exit_app
+  }
+
+  if option_methods.key?(number)
+    send(option_methods[number], app)
   else
-    exit
+    puts 'Enter valid number'
+    main(app)
   end
 end
 
-def main
-  @app = App.new
-
-  puts 'Welcome to School Library App!'
-  loop do
-    show_menu
-    user_input = gets.chomp
-    if user_input == '7'
-      puts 'Thank you for using this app!'
-      break
-    end
-    process_options(user_input)
-  end
+def exit_app(*)
+  puts 'Thank you for using this App'
 end
-main
+
+def create_person_option(app)
+  print 'Do you want to create student (1) or a teacher (2)? [Input the number]: '
+  person_code = gets.chomp.to_i
+  app.create_person(person_code)
+  main(app)
+end
+
+def create_book_option(app)
+  print 'Title: '
+  title = gets.chomp
+  print 'Author: '
+  author = gets.chomp
+  app.create_book(title, author)
+  main(app)
+end
+
+def get_rental_option(app)
+  print 'ID of person: '
+  id = gets.chomp.to_i
+  app.get_rental(id)
+  main(app)
+end
+
+def list_books(app)
+  app.list_books
+  main(app)
+end
+
+def list_persons(app)
+  app.list_persons
+  main(app)
+end
+
+def create_rental(app)
+  app.create_rental
+  main(app)
+end
+
+puts 'Welcome to School Library App!'
+main(app)
