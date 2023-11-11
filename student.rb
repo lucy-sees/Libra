210d8b72ba683eacea8623d48ec62e277e535824
Lucy W. Mwangi <100368317+lucy-sees@users.noreply.github.com>
2023-11-11 20:26:02 +0300

    Update student.rb

--- student.rb
+++ student.rb
@@ -3,19 +3,17 @@ require './person'
 class Student < Person
   attr_reader :classroom
 
-  def initialize(age, classroom, name = 'Unknown', parent_permission: true)
-    super(age, name, parent_permission: parent_permission)
-    @classroom = classroom
-  end
-
-  def play_hooky
-    '¯\(ツ)/¯'
+  def initialize(age, parent_permission, name = 'Unknown')
+    super(age, name)
+    @parent_permission = parent_permission
   end
 
   def classroom=(classroom)
     @classroom = classroom
-    classroom.students.push(self) unless
+    classroom.students.push(self) unless classroom.students.include?(self)
+  end
 
-    classroom.studemts.include?(self)
+  def play_hooky
+    '¯\(ツ)/¯'
   end
 end
