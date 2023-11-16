9fc79a54369f0833598b275bea4c23813a4d80ca
Lucy W. Mwangi <100368317+lucy-sees@users.noreply.github.com>
2023-11-16 15:17:12 +0300

    Create show_menu.rb

---
+++ show_menu.rb
@@ -0,0 +1,12 @@
+def show_menu
+  puts "\n\e[33m\e[2m===============================================\n\e[0m
+  \e[36mPlease choose an option by entering a number:
+  1 - List all books
+  2 - List all people
+  3 - Create a person
+  4 - Create a book
+  5 - Create a rental
+  6 - List all rentals for a given person id
+  \e[31m7 - Exit\e[0m"
+  print "\nYour choice: "
+end
