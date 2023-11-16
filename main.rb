c3e280b75eaf004daf64f932374062f38ada2f11
AbuTalha3 <127955219+AbuTalha3@users.noreply.github.com>
2023-11-16 18:18:16 +0500

    Remove linter errors

--- main.rb
+++ main.rb
@@ -9,16 +9,15 @@ def main
   @app.load_state
   puts "\e[1m\e[36mWelcome to School Library App!\e[0m"
 end
-  loop do
-    show_menu
-    user_input = gets.chomp.to_i
-    puts
-    if user_input == 7
-      @app.save_data_to_file
-      puts "\e[31mThank you for using this app! ♥️"
-      exit
-    end
-    process_options(user_input)
+loop do
+  show_menu
+  user_input = gets.chomp.to_i
+  puts
+  if user_input == 7
+    @app.save_data_to_file
+    puts "\e[31mThank you for using this app! ♥️"
+    exit
   end
+  process_options(user_input)
 end
 main
